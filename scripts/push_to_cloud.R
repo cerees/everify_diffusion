f64d9aa658e836fe677403a3aacd9f7c77f5dd46
cerees <cerees@albany.edu>
2015-05-04 16:25:33 -0400

    initial commit

--- scripts/push_to_cloud.R
+++ scripts/push_to_cloud.R
@@ -1,7 +1,7 @@
 #push to git
 system("pwd")
-#system("git init")
-#system("git add remote "https://github.com/cerees/segregation_and_neighborhood_health.git")
+system("git init")
+system("git remote add origin 'https://github.com/cerees/everify_diffusion'")
 system("git add .")
 system("git commit -m 'initial commit'")
 #system("git pull master origin")
